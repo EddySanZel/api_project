06acd958b7b124c01ab966b99cb91c2a0773ec20
Edilberto Sangay <esangayz16_1@unc.edu.pe>
2021-10-09 13:02:33 -0500

    Terminando autenticación y acceso de servicios

---
+++ controllers/auth.controller.js
@@ -0,0 +1,50 @@
+const { response } = require("express");
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/usuario.model');
+const { generarJWT } = require('../helpers/jwt');
+
+
+const login = async(req, res = response) => {
+    const { email, password } = req.body;
+
+    try {
+
+
+        //Verificar al usuario por email
+        const usuarioDB = await Usuario.findOne({ email });
+
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+                    //considerar la utilización de este mensaje
+            });
+        }
+        // Verificar constraseña
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Contraseña no válida'
+            });
+        }
+
+        // Generar el TOKEN - JWT
+        const token = await generarJWT(usuarioDB.id);
+
+        res.json({
+            ok: true,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+}
+
+module.exports = {
+    login
+}
--- index.js
+++ index.js
@@ -11,18 +11,16 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.json());
 
 // Estableciendo conexion a la BD
 dbConection();
 //console.log(process.env);
 
 //Rutas de la API Proyectos
-app.get('/', (req, res) => {
-    res.status(400).json({
-        ok: true,
-        msg: 'Bienvenidos a NodeJS'
-    })
-})
+app.use('/api/usuarios', require('./routes/usuarios.routes'));
+app.use('/api/login', require('./routes/auth.routes'));
+//app.use('/api/investigadores', require('./routes/investigadores.routes'));
 
 //Codigo para desplegar el servidor
 app.listen(process.env.PORT, () => {
